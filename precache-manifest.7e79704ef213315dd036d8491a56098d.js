eaf424c5bf03e78738360d7fb4de0f24561e88f6
Alina Kizilova <alinakzilova@gmail.com>
2019-09-24 02:36:03 +0300

    Updates

---
+++ precache-manifest.7e79704ef213315dd036d8491a56098d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0d7a14b834d023084d71500da89e0b",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "ef826bfb31c503d4b1de",
+    "url": "/react_dynamic-list-of-posts/static/css/main.2342775a.chunk.css"
+  },
+  {
+    "revision": "458e97df46596ee9ca41",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a3e16a20.chunk.js"
+  },
+  {
+    "revision": "ef826bfb31c503d4b1de",
+    "url": "/react_dynamic-list-of-posts/static/js/main.ffd19019.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
